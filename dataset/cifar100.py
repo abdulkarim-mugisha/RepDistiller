fcce5923a4dce08cfe104495f8850aad7fbb52fd
abdulkarim-mugisha <amugisha200@gmail.com>
2026-03-08 11:31:05 -0700

    crd update + new data from exps

--- dataset/cifar100.py
+++ dataset/cifar100.py
@@ -106,12 +106,8 @@ class CIFAR100InstanceSample(datasets.CIFAR100):
         self.is_sample = is_sample
 
         num_classes = 100
-        if self.train:
-            num_samples = len(self.train_data)
-            label = self.train_labels
-        else:
-            num_samples = len(self.test_data)
-            label = self.test_labels
+        num_samples = len(self.data)
+        label = self.targets
 
         self.cls_positive = [[] for i in range(num_classes)]
         for i in range(num_samples):
@@ -136,13 +132,7 @@ class CIFAR100InstanceSample(datasets.CIFAR100):
         self.cls_negative = np.asarray(self.cls_negative)
 
     def __getitem__(self, index):
-        if self.train:
-            img, target = self.train_data[index], self.train_labels[index]
-        else:
-            img, target = self.test_data[index], self.test_labels[index]
-
-        # doing this so that it is consistent with all other datasets
-        # to return a PIL Image
+        img, target = self.data[index], self.targets[index]
         img = Image.fromarray(img)
 
         if self.transform is not None:
